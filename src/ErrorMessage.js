351120c678f4503f2990e8d557265080b7075c33
Dale Anderson <danderson00@gmail.com>
2020-02-14 18:05:02 +1100

    Fix error message handling and export

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -1,6 +1,12 @@
 import { createElement } from 'react'
 
+export const extractErrorMessage = error => error
+  ? error.hasOwnProperty('stack')
+    ? error.message || 'An error occurred'
+    : error
+  : 'An error occurred'
+
 export default ({ error }) => createElement('div', {
   className: 'react-forms-error',
-  children: error ? error.message || error : undefined
+  children: extractErrorMessage(error)
 })
